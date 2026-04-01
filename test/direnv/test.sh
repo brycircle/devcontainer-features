974586763c6cfb9fda25359e75237f9febca9c3e
bryan hunt <bryan@livecircle.org>
2026-04-01 12:49:03 +0100

    Update direnv tests for Alpine compatibility

--- test/direnv/test.sh
+++ test/direnv/test.sh
@@ -3,14 +3,6 @@
 # This test file will be executed against an auto-generated devcontainer.json that
 # includes the 'direnv' feature with no options.
 #
-# Eg:
-# {
-#    "image": "<..some-base-image...>",
-#    "features": {
-#      "direnv": {}
-#    }
-# }
-#
 # This test can be run with the following command (from the root of this repo)
 #    devcontainer features test \
 #               --features direnv\
@@ -18,15 +10,14 @@
 
 set -e
 
-# Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-# Feature-specific tests
-# The 'check' command comes from the dev-container-features-test-lib.
-check "version" bash -c "direnv help | grep direnv | head -1"
-check "bash-integration" bash -c "cat ~/.bashrc | grep _direnv_hook\(\)"
-check "zsh-integration" bash -c "cat ~/.zshrc | grep _direnv_hook\(\)"
+check "version" direnv version
+check "bash-integration" grep "_direnv_hook" ~/.bashrc
 
-# Report result
-# If any of the checks above exited with a non-zero exit code, the test will fail.
+# zsh integration is only present when zsh is installed
+if command -v zsh >/dev/null 2>&1; then
+    check "zsh-integration" grep "_direnv_hook" ~/.zshrc
+fi
+
 reportResults
